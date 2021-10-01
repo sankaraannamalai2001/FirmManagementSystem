4845fedc9a07a6127518efa0cb90b9d7a7ddb0da
sankaraannamalai2001 <81792506+sankaraannamalai2001@users.noreply.github.com>
2021-10-01 16:34:03 +0530

    bg img

--- figma/test.py
+++ figma/test.py
@@ -16,8 +16,15 @@ class Login:
         self.bg = ImageTk.PhotoImage(file="background.png")
         self.bg_image = Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
         frame_login = Frame(self.root, bg="white")
-        frame_login.place(x=400, y=100, width=800, height=600)
+        frame_login.place(x=0, y=0, width=1600, height=800)
+        canvas1 = Canvas(frame_login, width=1600,
+                         height=800)
 
+        canvas1.pack(fill="both", expand=True)
+
+        # Display image
+        canvas1.create_image(0, 0, image=self.bg,
+                             anchor="nw")
         self.username = Entry(frame_login, text="Username", font=("poppins", 25), bg="#DBFFFA")
         self.username.place(x=670, y=280, width=475, height=52)
 
