3a8250a5ba1d6b57374ad4410e9c635e4e9ca443
prathishbv <bvprathish@gmail.com>
2021-10-01 17:32:20 +0530

    login and register page without database process continued.

--- figma/login.py
+++ figma/login.py
@@ -11,13 +11,13 @@ class Login:
     def __init__(self, root):
         self.root = root
         self.root.title("Login page")
-        self.root.geometry("1600x800")
+        self.root.geometry("1600x850")
         self.root.resizable(True, True)
         self.bg = ImageTk.PhotoImage(file="background.png")
         self.bg_image = Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
 
         frame_login = Frame()
-        frame_login.place(x=0, y=0, width=1600, height=800)
+        frame_login.place(x=0, y=0, width=1600, height=850)
         canvas1 = Canvas(frame_login, width=1600,
                          height=800)
 
@@ -33,18 +33,21 @@ class Login:
         self.password.place(x=650, y=380, width=475, height=52)
 
 
-        submit = Button(frame_login, command=self.check_function, text="LOGIN", bd=0, font=("poppins", 20, "bold"),
+        submit = Button(frame_login, command=self.login1, text="LOGIN", bd=0, font=("poppins", 20, "bold"),
                         bg="#DBFFFA", fg="#40ACB2").place(x=740, y=500, width=291, height=61)
-        submit = Button(frame_login, command=self.check_function, text="SIGNUP", bd=0, font=("poppins", 20, "bold"),
+        submit = Button(frame_login, command=self.signup1, text="SIGNUP", bd=0, font=("poppins", 20, "bold"),
                         bg="#DBFFFA", fg="#40ACB2").place(x=740, y=650, width=291, height=61)
 
 
-    def check_function(self):
+    def login1(self):
         if self.username.get() == "" or self.password.get() == "":
             messagebox.showerror("Error", "All fields are required", parent=self.root)
         else:
-            self.username.destroy()
+            self.username = ""
             self.root.after(2000, register.Register(self.root))
+
+    def signup1(self):
+       self.root.after(2000, register.Register(self.root))
 
 root = Tk()
 obj = Login(root)
--- figma/register.py
+++ figma/register.py
@@ -13,14 +13,14 @@ class Register:
     def __init__(self, root):
         self.root = root
         self.root.title("Login page")
-        self.root.geometry("1600x800")
+        self.root.geometry("1600x850")
         self.root.resizable(True, True)
         self.bg = ImageTk.PhotoImage(file="background1.png")
         self.bg_image = Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
         frame_register = Frame(self.root, bg="white")
         frame_register.place(x=0, y=0, width=1600, height=800)
         canvas1 = Canvas(frame_register, width=1600,
-                         height=800)
+                         height=850)
 
         canvas1.pack(fill="both", expand=True)
         canvas1.create_image(-50, -50, image=self.bg,
