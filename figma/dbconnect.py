ad93d1fdb0e7dbdd631b0bc9bbbc1f9879159d62
sankaraannamalai2001 <81792506+sankaraannamalai2001@users.noreply.github.com>
2021-10-05 12:00:41 +0530

    db conn to cluster

--- figma/dbconnect.py
+++ figma/dbconnect.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 def get_database():
-    #CONNECTION_STRING = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
-    CONNECTION_STRING = "mongodb+srv://firm:firm@cluster0.sakjm.mongodb.net/test"
+    CONNECTION_STRING = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+    #CONNECTION_STRING = "mongodb+srv://firm:firm@cluster0.sakjm.mongodb.net/test"
     client = MongoClient(CONNECTION_STRING)
     return client['Firm']
 db=get_database()
