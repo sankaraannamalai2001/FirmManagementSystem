62a265660e80bbaf4a2a062d3e966443729c9548
prathishbv <bvprathish@gmail.com>
2021-10-07 14:15:30 +0530

    OTP

--- figma/test.py
+++ figma/test.py
@@ -1,58 +1,35 @@
-from tkinter import *
-import tkinter as tk
-from tkinter import messagebox, ttk
-# import mysql.connector as mysql
-from PIL import ImageTk
-import smtplib
-from tkinter import scrolledtext
-import dbconnect
-import login
+import requests
+import math, random
 
+digits = "0123456789"
+OTP = ""
 
-class Detail:
-    def __init__(self,root):
-        self.root=root
-        self.root.title("Login page")
-        self.root.geometry("1600x850")
-        self.root.resizable(True, True)
-        self.bg = ImageTk.PhotoImage(file="articledetails2.png")
-        self.bg_image = Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
-        frame_details = Frame(self.root, bg="white")
-        frame_details.place(x=0, y=0, width=1600, height=800)
+# length of password can be changed
+# by changing value in range
+for i in range(4):
+    OTP += digits[math.floor(random.random() * 10)]
 
-        canvas1 = Canvas(frame_details, width=1600,
-                         height=850)
-        frame_inner = Frame(frame_details, bg="#aceae3")
-        frame_inner.place(x=450, y=200, width=400, height=250)
+otp = OTP
+print(otp)
 
-        canvas1.pack(fill="both", expand=True)
-        canvas1.create_image(-50, -50, image=self.bg,
-                             anchor="nw")
+url = "https://www.fast2sms.com/dev/bulkV2"
 
-        style = ttk.Style()
-        style.configure("mystyle.Treeview", highlightthickness=0, bd=0,
-                        font=('poppins', 11))  # Modify the font of the body
-        style.configure("mystyle.Treeview.Heading", font=('poppins', 13, 'bold'))  # Modify the font of the headings
-        style.layout("mystyle.Treeview", [('mystyle.Treeview.treearea', {'sticky': 'nswe'})])  # Remove the borders
+querystring = {
+    "authorization": "JLEw8Sjmh0VQaPUd7NfgyOX2ixbGrqHI1DMcA4pBCzs5tvnW9Ke3gLvcS0a4fjGAilUsJRw5yECdpKko",
+    "message": otp,
+    "language": "english",
+    "route": "q",
+    "numbers": "6382519268"
+    }
 
+headers = {
+    'cache-control': "no-cache"
+}
+try:
+    response = requests.request("GET", url,
+                                headers=headers,
+                                params=querystring)
 
-
-
-        self.tree = ttk.Treeview(frame_inner, column=("c1","c2"), show='headings',style="mystyle.Treeview")
-        self.tree.column("#1", anchor=tk.CENTER)
-        self.tree.heading("#1", text="Name")
-        self.tree.column("#2", anchor=tk.CENTER)
-        self.tree.heading("#2", text="Phone number")
-        # self.tree.column("#3", anchor=tk.CENTER)
-        # self.tree.heading("#3", text="Appointment Time")
-
-        self.tree.pack()
-        articles=dbconnect.col.find()
-        for art in articles:
-            print(art["username"])
-            self.tree.insert("", tk.END, values=[art["username"],art["phone"]])
-        # for row in records:
-        #
-root = Tk()
-obj = Detail(root)
-root.mainloop()
+    print("SMS Successfully Sent")
+except:
+    print("Oops! Something wrong")
