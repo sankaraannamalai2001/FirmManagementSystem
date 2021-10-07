a93b119fd9ef913a0967303a668c86cc3ef9fb0e
prathishbv <bvprathish@gmail.com>
2021-10-07 14:25:41 +0530

    OTP

--- figma/test.py
+++ figma/test.py
@@ -1,26 +1,34 @@
-import requests
-import math, random
+# import requests
+# import math, random
+#
+# digits = "0123456789"
+# OTP = ""
+#
+# # length of password can be changed
+# # by changing value in range
+# for i in range(4):
+#     OTP += digits[math.floor(random.random() * 10)]
+#
+# otp = OTP
+# print(otp)
 
+
+import requests, math, random
+
+url = "https://www.fast2sms.com/dev/bulkV2"
 digits = "0123456789"
 OTP = ""
-
-# length of password can be changed
-# by changing value in range
 for i in range(4):
     OTP += digits[math.floor(random.random() * 10)]
 
 otp = OTP
 print(otp)
-
-url = "https://www.fast2sms.com/dev/bulkV2"
-
 querystring = {
-    "authorization": "JLEw8Sjmh0VQaPUd7NfgyOX2ixbGrqHI1DMcA4pBCzs5tvnW9Ke3gLvcS0a4fjGAilUsJRw5yECdpKko",
+    "authorization": "ysBq4GOZykH2OrmqabVvHiEfy61jEidM5XPWg4ebaaMbJbUlBkOlhr0qZjoa",
     "message": otp,
     "language": "english",
     "route": "q",
-    "numbers": "6382519268"
-    }
+    "numbers": "6383519268"}
 
 headers = {
     'cache-control': "no-cache"
